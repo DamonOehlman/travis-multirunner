be12f64e0d5dacca0eab8515ddbbb4dfe0c99f96
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-26 17:09:08 +1100

    Trial a different approach for getting the correct SCRIPTPATH

--- start-firefox.sh
+++ start-firefox.sh
@@ -5,10 +5,7 @@ FIREFOX_HOME=$HOME/.mozilla/firefox
 
 # determine the script path
 # ref: http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
-
+SCRIPTPATH=$(dirname "$(readlink -f $0)")
 echo "firefox start scriptpath = $SCRIPTPATH"
 
 # set the prefs location
