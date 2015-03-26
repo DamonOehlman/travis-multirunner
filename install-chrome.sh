a4fa6e0b3645ebeb84cbcf502a0332c262519b36
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-26 13:15:27 +1100

    Created new installer scripts

---
+++ install-chrome.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+FNAME=`echo $1 | sed -r "s/^.*\/([^\/]*)$/\1/"`
+
+# cleanup any old files (shouldn't happen on travis)
+rm -rf $FNAME
+rm -rf ./browser-tmp
+
+# get the files
+wget $1
+dpkg -X $FNAME ./browser-tmp
+
+# make the target directory
+mkdir -p $2
+mv ./browser-tmp/opt/google/chrome/* $2
---
+++ install-firefox.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+FNAME=`echo $1 | sed -r "s/^.*\/([^\/]*)$/\1/"`
+
+# cleanup any old files (shouldn't happen on travis)
+# rm -rf $FNAME
+rm -rf ./browser-tmp
+mkdir -p ./browser-tmp
+
+# get the files
+# wget $1
+tar xvf $FNAME --directory ./browser-tmp
+
+# # make the target directory
+mkdir -p $2
+mv ./browser-tmp/firefox/* $2
+# echo $1 $2
