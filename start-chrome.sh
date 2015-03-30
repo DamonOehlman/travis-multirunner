5b786d90549b55b78a128aeb51f2e3722ad27885
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-30 13:52:27 +1100

    Improvements to the start-chrome script

--- start-chrome.sh
+++ start-chrome.sh
@@ -2,13 +2,20 @@
 BVER=${BVER-stable}
 DEBUG_OPTS="--enable-logging --v=1 --vmodule=*third_party/libjingle/*=3,*=0"
 EXTRA_OPTS="--no-sandbox --disable-setuid-sandbox --allow-sandbox-debugging"
+UUID=$(cat /proc/sys/kernel/random/uuid)
+BROWSER_COMMAND=~/bin/chrome-$BVER
+SCRIPTPATH=$(dirname "$(readlink -f $0)")
 
 #  --log-level=3 \
 #  --use-fake-device-for-media-stream \
 #  --use-fake-ui-for-media-stream \
 
-~/bin/chrome-$BVER \
-  --disable-setuid-sandbox \
+if [ ! -e $BROWSER_COMMAND ]; then
+  echo "could not find chrome executable: $BROWSER_COMMAND\n";
+  exit 1;
+fi
+
+$BROWSER_COMMAND --disable-setuid-sandbox \
   --console \
-  --no-first-run $@ \
-  --user-data-dir
+  --user-data-dir=$SCRIPTPATH/profiles/$(UUID)/ \
+  --no-first-run $@
