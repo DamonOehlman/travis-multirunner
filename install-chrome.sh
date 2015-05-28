ecc749dcaa6664611fb9f7ffefdaa6a96c82c3aa
Harald Alvestrand <hta@google.com>
2015-05-28 12:29:03 +0200

    Move downloaded .deb files into browser-tmp
    This prevents them cluttering up the working directory.

--- install-chrome.sh
+++ install-chrome.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+# Install chrome.
+# Arguments: $1=target URL, $2=target path
+#
+# The script expects to run in the script home dir, and uses
+# the subdir "browser-tmp" as a temporary directory.
+#
 FNAME=`echo $1 | sed -r "s/^.*\/([^\/]*)$/\1/"`
 
 # cleanup any old files (shouldn't happen on travis)
-rm -rf $FNAME
 rm -rf ./browser-tmp
+mkdir ./browser-tmp
 
 # get the files
-wget $1
-dpkg -X $FNAME ./browser-tmp
+(cd ./browser-tmp; wget $1)
+dpkg -X ./browser-tmp/$FNAME ./browser-tmp
 
 # remove the broken link
 rm -rf ./browser-tmp/opt/google/chrome*/google-chrome
@@ -15,3 +21,4 @@ rm -rf ./browser-tmp/opt/google/chrome*/google-chrome
 # make the target directory
 mkdir -p $2
 mv ./browser-tmp/opt/google/chrome*/* $2
+rm -rf ./browser-tmp
