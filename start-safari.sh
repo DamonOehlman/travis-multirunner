076e5ff493c4efa6c81d932b3c8467e071580088
Adam Ullman <adam@tokbox.com>
2017-06-19 15:07:59 +1000

    Open Safari Tech Preview first and then copy the permissions file over
    once the directory has been created

--- start-safari.sh
+++ start-safari.sh
@@ -15,6 +15,13 @@ defaults write com.apple.Safari WebKitMockCaptureDevicesEnabled 1
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2MockCaptureDevicesEnabled 1
 
 # Allow device access
+# This UserMediaPermissions.plist file allows 127.0.0.1 and localhost. To add other domains
+# you need to run Safari Technology Preview on your system, visit the domain you want to allow
+# and call navigator.mediaDevices.getUserMedia. When prompted allow access to devices allow access
+# then click on the red camera in the URL bar and choose "Always Allow".
+# Finally copy the `~/Library/SafariTechnologyPreview/UserMediaPermissions.plist` file into the
+# same directory on Travis.
+open -a "$SAFARI_NAME"
 cp ./UserMediaPermissions.plist ~/Library/SafariTechnologyPreview/
 
 open -a "$SAFARI_NAME" $@
